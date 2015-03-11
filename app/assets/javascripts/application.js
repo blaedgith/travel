6175a8ea0807346a6f14d0a983de496d0c36f0f1
Romina Painter <rominapalacios@me.com>
2015-03-11 08:54:35 -0400

    added figaro gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,41 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
 //= require_tree .
+
+$(document).ready(function () {
+	function initialize(){
+
+		var myLatlng = new google.maps.LatLng(45.542464, -80.654422)
+
+
+		var mapOptions = {
+			center: myLatlng,
+			zoom: 8,
+			scrollWheel: false
+		};
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			animation: google.maps.Animation.DROP,
+			title: 'Machu Picchu'
+		});
+
+		var contentString = '<h2> Machu Picchu</h2>' + '<p>This is the coolest place on Earth you guys</p>';
+		var infoWindow = new google.maps.Infowindow ({
+			content: contentString
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+
+});
